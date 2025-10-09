7324069e43327f3cc3ce098f438a18df6e4a2161
Thang Nguyen <125545428+NgVThangBz@users.noreply.github.com>
2025-10-09 17:10:25 +0700

    add AsyncTaskPool

--- axmol/axmol.h
+++ axmol/axmol.h
@@ -38,6 +38,7 @@ THE SOFTWARE.
 
 // base
 #include "axmol/base/AutoreleasePool.h"
+#include "axmol/base/AsyncTaskPool.h"
 #include "axmol/base/Configuration.h"
 #include "axmol/base/Logging.h"
 #include "axmol/base/Data.h"
